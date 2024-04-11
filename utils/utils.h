f73becf73bb30d4f93be4002f89d8393cc547ca3
2202210 <2202210@sit.singaporetech.edu.sg>
2024-04-12 04:58:06 +0800

    initial commit

---
+++ utils/utils.h
@@ -0,0 +1,24 @@
+
+#ifndef UTILS_H
+# define UTILS_H
+
+// libraries required
+#include <readline/readline.h>
+#include <readline/history.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <signal.h>
+#include <unistd.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <termios.h>
+#include <termcap.h>
+
+// libft
+# include "../../libft/libft.h"
+
+#endif
